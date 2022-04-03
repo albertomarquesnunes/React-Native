5da3bc0f650d9fc52ab49e54935c101739d17b65
Alberto Marques Nunes <nunes.alberto@gmail.com>
2022-04-03 18:44:06 -0300

    App10 terminado e pronto pra exame da P1

--- App10/App.js
+++ App10/App.js
@@ -1,50 +1,63 @@
-import React, { Component } from 'react';
-import {SafeAreaView, ActivityIndicator, FlatList, Text, View } from 'react-native';
+import React from "react";
+import {ScrollView, SafeAreaView ,Text} from "react-native";
+import { styles } from "./styles";
+import Conversor from "./src/conversor";
+import {Picker} from '@react-native-picker/picker';
 
-export default class App extends Component {
-  constructor(props) {
+export default class App extends React.Component {
+  constructor(props){
     super(props);
-
     this.state = {
-      dataEuro: [],
-      dataDolar:[],
-      isLoading: true
-    };
-  }
+       MoedaA: 0,
+        MoedasA: [
+          {nome: 'Dólar', sigla:'USD'},
+          {nome: 'Euro', sigla:'EUR'},
+          {nome: 'Real', sigla:'BRL'}
+               ],
+      MoedaB: 0,
+        MoedasB: [
+          {nome: 'Real', sigla:'BRL'},
+          {nome: 'Dólar', sigla:'USD'},
+          {nome: 'Euro', sigla:'EUR'}
+         
+          ]
+   };
   
+}
+
 
   render() {
-    const { dataEuro,dataDolar, isLoading } = this.state;
-console.log(dataDolar);
-console.log(dataEuro);
+    let MoedaAItem = this.state.MoedasA.map( (valor, chave) => {
+      return <Picker.Item key={chave} value={chave} label={valor.nome} />
+    })
+    let MoedaBItem = this.state.MoedasB.map( (valor, chave) => {
+      return <Picker.Item key={chave} value={chave} label={valor.nome} />
+    })
     return (
-      
-      <SafeAreaView style={{ flex: 1, padding: 24 }}>
-        {isLoading ? <ActivityIndicator/> : <Text></Text>}
-      </SafeAreaView>
-      
-      
-    );
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+         <Text style={styles.title} >
+        Conversor de Moedas Dólar, Real e Euro
+        </Text>
+       
+    <Text style={styles.texto}>De:</Text>
+    <Picker style={styles.picker} itemStyle={styles.pickerItem} selectedValue={this.state.MoedaA}
+    onValueChange={ (itemValue, itemIndex) => this.setState({MoedaA: itemValue}) }
+    >
+           {MoedaAItem}
+    </Picker>
     
+    <Text style={styles.texto}>Para: </Text>
+      <Picker style={styles.picker} itemStyle={styles.pickerItem} selectedValue={this.state.MoedaB}
+      onValueChange={ (itemValue, itemIndex) => this.setState({MoedaB: itemValue}) }
+      >
+             {MoedaBItem}
+       </Picker>
+     
+        <Conversor coinA={this.state.MoedasA[this.state.MoedaA].sigla} coinB={this.state.MoedasB[this.state.MoedaB].sigla} />
+        </ScrollView>
+      </SafeAreaView>
+    );
   }
-  componentDidMount() {
-    this.getValores();
-  }
-  async getValores() {
-    try {
-      const responseEuro = await fetch('https://economia.awesomeapi.com.br/EUR-BRL/1?format=json');
-      const responseDolar = await fetch('https://economia.awesomeapi.com.br/USD-BRL/1?format=json');
-      const jsonEuro = await responseEuro.json();
-      const jsonDolar = await responseDolar.json();
-      this.setState({ dataEuro: jsonEuro ,dataDolar: jsonDolar});
-      
-      
-    } catch (error) {
-      console.log(error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-  
+}
 
-};
--- App10/styles.js
+++ App10/styles.js
@@ -1,86 +1,52 @@
 import {StyleSheet} from 'react-native';
- 
+
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        margin: 25,
-        marginTop: 15,
-      },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+   },
+  picker:{
+    fontSize: 150,
+    borderWidth:11,
+    width: 150 ,
+    borderWidth:1,
+    borderColor: 'black',
+    alignSelf:'auto',
+    alignItems: 'baseline',
+    marginTop: -25,
+    marginLeft:80,
+   
 
-      sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-      }, 
-
-      input:{
-        borderColor: "gray",
-        borderWidth: 1,
-        borderRadius: 10,
-        padding: 10,
-        fontSize: 20,
-        
-      },
-    picker:{
-        fontSize: 150,
-        borderWidth:1,
-        height: 50, 
-        width: 150 ,
-        width: 200,
-        borderColor: 'black',
-        width: "100%",
-        
-    },
-    pickerItem: {
-        height: 44,
-    },
+  },
+texto:{
+  color: '#000000',
+  margin: 10,
+  fontSize: 20,
+   width: 60,
+  alignSelf:'auto',
+  padding: 10,
+  marginBottom:-20
+},
+pickerItem: {
+    height: 44,
+}, 
+title: {
+  fontSize: 30,
+  fontWeight: "bold",
+  color: "#ff0000",
+  alignSelf:"center",
+  margin: 30,
+},
+safe:{
+  flex: 0.5,
+  alignContent:'center',
+  alignSelf:'center',
+  borderWidth:1,
+  width: 340,
+}
   
-    texto:{
-      margin: 10,
-      color: '#000000',
-      margin: 10,
-      fontSize: 20,
-       
-    },
-    titulo:{
-      color: '#ff0000',
-      alignSelf: "center",
-      textAlign: 'center',
-      textAlignVertical:'center',
-      height: 80,
-      fontSize: 30,
-      fontWeight:"bold"
-     
-    },
-  
-    titulo2:{
-      color: '#0000ff',
-      textAlign: 'left',
-      textAlignVertical:'center',
-      height: 80,
-      margin: 5,
-      fontSize: 30,
-      fontWeight:"bold"
-     
-    },
+});
 
-    botao:{
-        alignSelf: "center",
-        width: 250,
-        height: 50,
-        margin: 10,
-        backgroundColor: '#0000cc'
-      },
-     
-      textoBotao:{
-        color: '#ffffff',
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        height: 45,
-        fontSize: 36,
-      },
-         
-  });
-
- 
- 
-  export {styles};
+export {styles};
